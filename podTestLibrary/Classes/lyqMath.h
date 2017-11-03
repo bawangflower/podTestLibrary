64ae1604495f2b4ae90cc8c8f11b611d153b898d
bawangflower <flower258@dingtalk.com>
2017-11-03 13:58:49 +0800

    add math

---
+++ podTestLibrary/Classes/lyqMath.h
@@ -0,0 +1,17 @@
+//
+//  lyqMath.h
+//  liyanqinMaths
+//
+//  Created by 李 燕琴 on 17/2/26.
+//  Copyright © 2017年 liyanqin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface lyqMath : NSObject
+
++ (NSInteger)addWithA:(NSInteger)A andB:(NSInteger)B;
+
++ (NSInteger)subWithA:(NSInteger)A andB:(NSInteger)B;
+
+@end
---
+++ podTestLibrary/Classes/lyqMath.m
@@ -0,0 +1,23 @@
+//
+//  lyqMath.m
+//  liyanqinMaths
+//
+//  Created by 李 燕琴 on 17/2/26.
+//  Copyright © 2017年 liyanqin. All rights reserved.
+//
+
+#import "lyqMath.h"
+
+@implementation lyqMath
+
++ (NSInteger)addWithA:(NSInteger)A andB :(NSInteger)B {
+    NSInteger C = A + B;
+    return C;
+}
+
++ (NSInteger)subWithA:(NSInteger)A andB :(NSInteger)B {
+    NSInteger C = A - B;
+    return C;
+}
+
+@end
